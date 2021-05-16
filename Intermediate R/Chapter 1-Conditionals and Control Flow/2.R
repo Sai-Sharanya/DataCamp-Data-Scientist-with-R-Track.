05d8ae976fc162ce7ee5bbc3929a8aec356442b0
Sai Sharanya Y <63389382+Sai-Sharanya@users.noreply.github.com>
2021-05-16 11:46:54 +0530

    Update 2.R

--- Intermediate R/Chapter 1-Conditionals and Control Flow/2.R
+++ Intermediate R/Chapter 1-Conditionals and Control Flow/2.R
@@ -1,4 +1,5 @@
-# Greater and less than
+#2
+#Greater and less than
 # Apart from equality operators, Filip also introduced the less than and greater than operators: < and >. You can also add an equal sign to express less than or equal to or greater than or equal to, respectively. Have a look at the following R expressions, that all evaluate to FALSE:
 #   
 #   (1 + 2) > 4
@@ -6,8 +7,8 @@
 # TRUE <= FALSE
 # Remember that for string comparison, R determines the greater than relationship based on alphabetical order. Also, keep in mind that TRUE is treated as 1 for arithmetic, and FALSE is treated as 0. Therefore, FALSE < TRUE is TRUE.
 # 
-# Instructions
-# 100 XP
+# Instructions :
+
 # Write R expressions to check whether:
 #   
 #   -6 * 5 + 2 is greater than or equal to -10 + 1.
