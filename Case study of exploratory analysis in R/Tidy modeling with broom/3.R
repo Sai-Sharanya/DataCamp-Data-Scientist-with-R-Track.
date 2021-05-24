cd2614ec495ea6ad9894cb167fe8e50ed81f2d65
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-05-24 21:04:16 +0530

    Update 3.R

--- Case study of exploratory analysis in R/Tidy modeling with broom/3.R
+++ Case study of exploratory analysis in R/Tidy modeling with broom/3.R
@@ -1,1 +1,20 @@
 #3
+
+
+
+# Linear regression of percent_yes by year for US
+US_by_year <- by_year_country %>%
+  filter(country == "United States")
+US_fit <- lm(percent_yes ~ year, US_by_year)
+
+# Fit model for the United Kingdom
+UK_by_year <- by_year_country %>%
+  filter(country == "United Kingdom")
+UK_fit <- lm(percent_yes ~ year, UK_by_year)
+
+# Create US_tidied and UK_tidied
+US_tidied <- tidy(US_fit)
+UK_tidied <- tidy(UK_fit)
+
+# Combine the two tidied models
+bind_rows(US_tidied, UK_tidied)
