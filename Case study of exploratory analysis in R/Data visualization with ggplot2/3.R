f5beff60dce4240c5d3514a302105202c45826dd
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-05-24 20:46:02 +0530

    Update 3.R

--- Case study of exploratory analysis in R/Data visualization with ggplot2/3.R
+++ Case study of exploratory analysis in R/Data visualization with ggplot2/3.R
@@ -1,1 +1,8 @@
 #3
+
+
+# Group by year and country: by_year_country
+by_year_country <- votes_processed %>%
+  group_by(year, country) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
