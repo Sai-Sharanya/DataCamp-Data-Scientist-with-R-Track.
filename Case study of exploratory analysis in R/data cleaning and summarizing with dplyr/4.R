2997913cafe19345fb4039b1b99609111aa757bb
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-05-24 19:45:19 +0530

    Update 4.R

--- Case study of exploratory analysis in R/data cleaning and summarizing with dplyr/4.R
+++ Case study of exploratory analysis in R/data cleaning and summarizing with dplyr/4.R
@@ -1,1 +1,10 @@
 #4
+
+
+# Print votes_processed
+votes_processed
+
+# Find total and fraction of "yes" votes
+votes_processed %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
