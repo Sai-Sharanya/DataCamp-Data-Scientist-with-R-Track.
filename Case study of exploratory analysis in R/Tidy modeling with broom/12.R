9a4a58547024858519c1f010f3eeda4c0a28512c
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-05-24 21:26:22 +0530

    Update 12.R

--- Case study of exploratory analysis in R/Tidy modeling with broom/12.R
+++ Case study of exploratory analysis in R/Tidy modeling with broom/12.R
@@ -1,1 +1,17 @@
 #12
+
+
+
+# Filter by adjusted p-values
+filtered_countries <- country_coefficients %>%
+  filter(term == "year") %>%
+  mutate(p.adjusted = p.adjust(p.value)) %>%
+  filter(p.adjusted < .05)
+
+# Sort for the countries increasing most quickly
+filtered_countries %>%
+  arrange(desc(estimate))
+
+# Sort for the countries decreasing most quickly
+filtered_countries %>%
+  arrange(estimate)
