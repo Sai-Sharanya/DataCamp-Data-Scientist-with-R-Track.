1f5a7a1496b5df886e36e96eaae595ed0e037d5c
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-05-24 21:13:50 +0530

    Update 8.R

--- Case study of exploratory analysis in R/Tidy modeling with broom/8.R
+++ Case study of exploratory analysis in R/Tidy modeling with broom/8.R
@@ -1,1 +1,11 @@
-#8
+#8\
+
+
+# Load the broom package
+library(broom)
+
+# Add another mutate that applies tidy() to each model
+by_year_country %>%
+  nest(-country) %>%
+  mutate(model = map(data, ~ lm(percent_yes ~ year, data = .)),
+         tidied = map(model, tidy))
