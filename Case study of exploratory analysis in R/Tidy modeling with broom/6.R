b8c6fdef8c83d6329da50d8a20ea906bf0e2ffe0
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-05-24 21:08:39 +0530

    Update 6.R

--- Case study of exploratory analysis in R/Tidy modeling with broom/6.R
+++ Case study of exploratory analysis in R/Tidy modeling with broom/6.R
@@ -1,1 +1,11 @@
 #6
+
+
+
+# All countries are nested besides country
+nested <- by_year_country %>%
+  nest(-country)
+
+# Unnest the data column to return it to its original form
+nested %>%
+  unnest(data)
