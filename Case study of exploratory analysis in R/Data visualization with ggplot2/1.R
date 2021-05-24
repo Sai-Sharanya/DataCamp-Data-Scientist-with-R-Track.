8ad26afbd4c6edeebb1822f236288615b51a83bc
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-05-24 20:44:44 +0530

    Update 1.R

--- Case study of exploratory analysis in R/Data visualization with ggplot2/1.R
+++ Case study of exploratory analysis in R/Data visualization with ggplot2/1.R
@@ -1,1 +1,14 @@
 #1
+
+# Define by_year
+by_year <- votes_processed %>%
+  group_by(year) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+# Load the ggplot2 package
+library(ggplot2)
+
+# Create line plot
+ggplot(by_year, aes(year, percent_yes)) +
+  geom_line()
