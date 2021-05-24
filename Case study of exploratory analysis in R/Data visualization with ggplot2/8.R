fcc2c3ce122faf6fdf660923e071f062b1310da2
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-05-24 20:49:59 +0530

    Update 8.R

--- Case study of exploratory analysis in R/Data visualization with ggplot2/8.R
+++ Case study of exploratory analysis in R/Data visualization with ggplot2/8.R
@@ -1,1 +1,17 @@
 #8
+
+
+
+# Add three more countries to this list
+countries <- c("United States", "United Kingdom",
+               "France", "Japan", "Brazil", "India",
+               "New Zealand", "Australia", "Poland")
+
+# Filtered by_year_country: filtered_countries
+filtered_countries <- by_year_country %>%
+  filter(country %in% countries)
+
+# Line plot of % yes over time faceted by country
+ggplot(filtered_countries, aes(year, percent_yes)) +
+  geom_line() +
+  facet_wrap(~ country, scales = "free_y")
