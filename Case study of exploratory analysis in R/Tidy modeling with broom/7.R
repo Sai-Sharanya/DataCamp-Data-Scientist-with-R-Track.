efba7c5b9dfcaf1b14e3669aed115f5e8bd1fd9a
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-05-24 21:13:09 +0530

    Update 7.R

--- Case study of exploratory analysis in R/Tidy modeling with broom/7.R
+++ Case study of exploratory analysis in R/Tidy modeling with broom/7.R
@@ -1,1 +1,11 @@
 #7
+
+
+# Load tidyr and purrr
+library(tidyr)
+library(purrr)
+
+# Perform a linear regression on each item in the data column
+by_year_country %>%
+  nest(-country) %>%
+  mutate(model = map(data, ~ lm(percent_yes ~ year, data = .)))
