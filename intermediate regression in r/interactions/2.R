10cadc0e3d87db4ddfb377e41edb90182d1693d9
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 13:45:58 +0530

    Update 2.R

--- intermediate regression in r/interactions/2.R
+++ intermediate regression in r/interactions/2.R
@@ -1,1 +1,30 @@
 #2
+
+
+
+# Create a tibble of explanatory data, setting
+# no. of conv stores to 0 to 10
+explanatory_data <- tibble(
+  n_convenience = 0:10
+)
+
+
+
+
+
+# From previous step
+explanatory_data <- tibble(
+  n_convenience = 0:10
+)
+
+# Add column of predictions using "0 to 15" model and explanatory data 
+prediction_data_0_to_15 <- explanatory_data %>% 
+  mutate(price_twd_msq = predict(mdl_0_to_15, explanatory_data))
+
+# Same again, with "15 to 30"
+prediction_data_15_to_30 <- explanatory_data %>% 
+  mutate(price_twd_msq = predict(mdl_15_to_30, explanatory_data))
+
+# Same again, with "30 to 45"
+prediction_data_30_to_45 <- explanatory_data %>% 
+  mutate(price_twd_msq = predict(mdl_30_to_45, explanatory_data))
