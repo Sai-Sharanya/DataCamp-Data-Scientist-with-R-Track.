d6932ee5036a94d9260f5c15d72c267483d13e91
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:22:15 +0530

    Update 10.R

--- Introduction to regression in R/Prediction and model objects/10.R
+++ Introduction to regression in R/Prediction and model objects/10.R
@@ -1,1 +1,68 @@
 #10
+
+
+
+# Run the code to see the plot
+# Edit to raise x, y aesthetics to power 0.25
+ggplot(ad_conversion, aes(n_impressions^0.25, n_clicks^0.25)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+
+
+# Run a linear regression of n_clicks to the power 0.25 vs. 
+# n_impressions to the power 0.25 using ad_conversion
+mdl_click_vs_impression <- lm(
+  I(n_clicks ^ 0.25) ~ I(n_impressions ^ 0.25),
+  data = ad_conversion
+)
+
+
+
+# From previous step
+mdl_click_vs_impression <- lm(
+  I(n_clicks ^ 0.25) ~ I(n_impressions ^ 0.25),
+  data = ad_conversion
+)
+
+# Use this explanatory data
+explanatory_data <- tibble(
+  n_impressions = seq(0, 3e6, 5e5)
+)
+
+prediction_data <- explanatory_data %>% 
+  mutate(
+    # Use mdl_click_vs_impression to predict n_clicks ^ 0.25
+    n_clicks_025 = predict(mdl_click_vs_impression, explanatory_data),
+    # Back transform to get n_clicks
+    n_clicks = n_clicks_025 ^ 4
+  )
+
+
+
+
+
+
+
+# From previous steps
+mdl_click_vs_impression <- lm(
+  I(n_clicks ^ 0.25) ~ I(n_impressions ^ 0.25),
+  data = ad_conversion
+)
+explanatory_data <- tibble(
+  n_impressions = seq(0, 3e6, 5e5)
+)
+prediction_data <- explanatory_data %>% 
+  mutate(
+    n_clicks_025 = predict(mdl_click_vs_impression, explanatory_data),
+    n_clicks = n_clicks_025 ^ 4
+  )
+
+ggplot(ad_conversion, aes(n_impressions ^ 0.25, n_clicks ^ 0.25)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  # Add points from prediction_data, colored green
+  geom_point(data = prediction_data, color = "green")
+
+
