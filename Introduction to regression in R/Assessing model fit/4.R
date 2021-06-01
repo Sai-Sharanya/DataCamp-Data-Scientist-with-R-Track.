c0e4b4c3f8b328988438704fb23e461c86f8d33a
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:58:18 +0530

    Update 4.R

--- Introduction to regression in R/Assessing model fit/4.R
+++ Introduction to regression in R/Assessing model fit/4.R
@@ -1,1 +1,28 @@
 #4
+
+mdl_price_vs_dist %>% 
+  # Augment the model
+  augment() %>% 
+  # Arrange rows by descending leverage
+  arrange(desc(.hat)) %>% 
+  # Get the head of the dataset
+  head()
+
+
+
+
+
+mdl_price_vs_dist %>% 
+  # Augment the model
+  augment() %>% 
+  # Arrange rows by descending Cook's distance
+  arrange(desc(.cooksd)) %>% 
+  # Get the head of the dataset
+  head()
+
+
+
+
+
+# Plot the three outlier diagnostics for mdl_price_vs_conv
+autoplot(mdl_price_vs_dist, which = 4:6, nrow = 3, ncol = 1)
