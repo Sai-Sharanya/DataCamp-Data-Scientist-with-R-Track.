0c74e61efc506966f0fa58f745855639d5859950
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 12:01:14 +0530

    Update 4.R

--- intermediate regression in r/parallel slopes/4.R
+++ intermediate regression in r/parallel slopes/4.R
@@ -1,1 +1,56 @@
 #4
+
+
+
+# Make a grid of explanatory data
+explanatory_data <- expand_grid(
+  # Set n_convenience to zero to ten
+  n_convenience = 0:10,
+  # Set house_age_years to the unique values of that variable
+  house_age_years = unique(taiwan_real_estate$house_age_years)
+)
+
+# See the result
+explanatory_data
+
+
+
+
+
+# From previous step
+explanatory_data <- expand_grid(
+  n_convenience = 0:10,
+  house_age_years = unique(taiwan_real_estate$house_age_years)
+)
+
+# Add predictions to the data frame
+prediction_data <- explanatory_data %>% 
+  mutate(
+    price_twd_msq=predict(mdl_price_vs_both, explanatory_data)
+  )
+
+# See the result
+prediction_data
+
+
+
+
+
+
+
+# From previous steps
+explanatory_data <- expand_grid(
+  n_convenience = 0:10,
+  house_age_years = unique(taiwan_real_estate$house_age_years)
+)
+prediction_data <- explanatory_data %>% 
+  mutate(
+    price_twd_msq = predict(mdl_price_vs_both, explanatory_data)
+  )
+
+taiwan_real_estate %>% 
+  ggplot(aes(n_convenience, price_twd_msq, color = house_age_years)) +
+  geom_point() +
+  geom_parallel_slopes(se = FALSE) +
+  # Add points using prediction_data, with size 5 and shape 15
+  geom_point(data = prediction_data, size = 5, shape = 15)
