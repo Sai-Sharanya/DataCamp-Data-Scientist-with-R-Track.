6ea42429ec9e04ff42cd4e732bc3e25045d69427
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:36:38 +0530

    Update 2.R

--- Introduction to regression in R/Assessing model fit/2.R
+++ Introduction to regression in R/Assessing model fit/2.R
@@ -1,1 +1,23 @@
 #2
+
+
+# Get RSE for mdl_click_vs_impression_orig
+mdl_click_vs_impression_orig %>% 
+  # Get the model-level details
+  glance() %>% 
+  # Pull out sigma
+  pull(sigma)
+
+# Do the same for the transformed model
+mdl_click_vs_impression_trans %>%
+  glance()  %>%
+   pull(sigma)
+
+
+
+
+
+#The typical difference between observed number of clicks and predicted number of clicks is 20
+
+
+#The transformed model, mdl_click_vs_impression_trans
