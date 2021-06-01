6f1931758e751aad4d6a5d096e25ff22b83b921a
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 11:48:56 +0530

    Update 2.R

--- intermediate regression in r/parallel slopes/2.R
+++ intermediate regression in r/parallel slopes/2.R
@@ -1,1 +1,17 @@
 #2
+
+
+# Using taiwan_real_estate, plot price_twd_msq vs. n_convenience
+ggplot(taiwan_real_estate, aes(n_convenience, price_twd_msq)) +
+  # Add a point layer
+  geom_point() +
+  # Add a smooth trend line using linear regr'n, no ribbon
+  geom_smooth(method = "lm", se = FALSE)
+
+
+
+
+# Using taiwan_real_estate, plot price_twd_msq vs. house_age_years
+ggplot(taiwan_real_estate, aes(house_age_years, price_twd_msq)) +
+  # Add a box plot layer
+  geom_boxplot()
