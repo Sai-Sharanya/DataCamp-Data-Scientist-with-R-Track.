cc561855dd8183c9c6f577d15132928f50498f04
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 16:13:59 +0530

    Update 5.R

--- intermediate regression in r/interactions/5.R
+++ intermediate regression in r/interactions/5.R
@@ -1,1 +1,13 @@
 #5
+
+
+# Model price vs both with an interaction using "times" syntax
+lm(price_twd_msq ~ n_convenience * house_age_years, data = taiwan_real_estate)
+
+
+
+# Model price vs both with an interaction using "colon" syntax
+lm(
+  price_twd_msq ~ n_convenience + house_age_years + n_convenience:house_age_years, 
+  data = taiwan_real_estate
+)
