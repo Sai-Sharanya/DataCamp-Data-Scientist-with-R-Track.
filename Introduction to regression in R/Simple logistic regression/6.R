6690c62d576a9b811d4ba4661d467cef1cf5d4a1
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 10:16:31 +0530

    Update 6.R

--- Introduction to regression in R/Simple logistic regression/6.R
+++ Introduction to regression in R/Simple logistic regression/6.R
@@ -1,1 +1,34 @@
 #6
+
+
+# Update the data frame
+prediction_data <- explanatory_data %>% 
+  mutate(   
+    has_churned = predict(
+      mdl_churn_vs_relationship, explanatory_data, 
+      type = "response"
+    ),
+    # Add the odds ratio
+    odds_ratio = has_churned / (1 - has_churned)
+  )
+
+# See the result
+prediction_data
+
+
+
+
+
+# From previous step
+prediction_data <- explanatory_data %>% 
+  mutate(   
+    has_churned = predict(mdl_churn_vs_relationship, explanatory_data, type = "response"),
+    odds_ratio = has_churned / (1 - has_churned)
+  )
+
+# Using prediction_data, plot odds_ratio vs. time_since_first_purchase
+ggplot(prediction_data, aes(time_since_first_purchase, odds_ratio)) +
+  # Make it a line plot
+  geom_line() +
+  # Add a dotted horizontal line at y = 1
+  geom_hline(yintercept = 1, linetype = "dotted")
