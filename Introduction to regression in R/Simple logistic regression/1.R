b20aa0b2b1cae6d21ecdefec660e3476786035e9
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 10:06:40 +0530

    Update 1.R

--- Introduction to regression in R/Simple logistic regression/1.R
+++ Introduction to regression in R/Simple logistic regression/1.R
@@ -1,1 +1,16 @@
 #1
+
+
+# Using churn, plot time_since_last_purchase
+ggplot(churn, aes(time_since_last_purchase)) +
+  # as a histogram with binwidth 0.25
+  geom_histogram(binwidth = 0.25) +
+  # faceted in a grid with has_churned on each row
+  facet_grid(rows = vars(has_churned))
+
+
+
+# Redraw the plot with time_since_first_purchase
+ggplot(churn, aes(time_since_first_purchase)) +
+  geom_histogram(binwidth = 0.25) +
+  facet_grid(rows = vars(has_churned))
