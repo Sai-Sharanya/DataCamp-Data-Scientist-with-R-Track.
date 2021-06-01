b2a9e8c8c36e8bdd60950168b5ac4370547e72e7
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:20:14 +0530

    Update 9.R

--- Introduction to regression in R/Prediction and model objects/9.R
+++ Introduction to regression in R/Prediction and model objects/9.R
@@ -1,1 +1,73 @@
 #9
+
+
+# Run the code to see the plot
+# Edit so x-axis is square root of dist_to_mrt_m
+ggplot(taiwan_real_estate, aes(sqrt(dist_to_mrt_m), price_twd_msq)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+
+
+# Run a linear regression of price_twd_msq vs. 
+# square root of dist_to_mrt_m using taiwan_real_estate
+mdl_price_vs_dist <- lm(formula=price_twd_msq~sqrt(dist_to_mrt_m), data= taiwan_real_estate)
+
+
+
+
+# See the result
+mdl_price_vs_dist
+
+
+
+
+
+# From previous step
+mdl_price_vs_dist <- lm(
+  price_twd_msq ~ sqrt(dist_to_mrt_m), 
+  data = taiwan_real_estate
+)
+
+# Use this explanatory data
+explanatory_data <- tibble(
+  dist_to_mrt_m = seq(0, 80, 10) ^ 2
+)
+
+# Use mdl_price_vs_dist to predict explanatory_data
+prediction_data <- explanatory_data %>% 
+  mutate(
+    price_twd_msq = predict(mdl_price_vs_dist, explanatory_data)
+  )
+
+# See the result
+prediction_data
+
+
+
+
+
+
+
+
+
+
+# From previous steps
+mdl_price_vs_dist <- lm(
+  price_twd_msq ~ sqrt(dist_to_mrt_m), 
+  data = taiwan_real_estate
+)
+explanatory_data <- tibble(
+  dist_to_mrt_m = seq(0, 80, 10) ^ 2
+)
+prediction_data <- explanatory_data %>% 
+  mutate(
+    price_twd_msq = predict(mdl_price_vs_dist, explanatory_data)
+  )
+
+ggplot(taiwan_real_estate, aes(sqrt(dist_to_mrt_m), price_twd_msq)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  # Add points from prediction_data, colored green, size 5
+  geom_point(data = prediction_data, color = "green", size = 5)
