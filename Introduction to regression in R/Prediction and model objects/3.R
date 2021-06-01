2c8ee1d4482b4f98fb50ee45b46d10a8a2455038
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:14:52 +0530

    Update 3.R

--- Introduction to regression in R/Prediction and model objects/3.R
+++ Introduction to regression in R/Prediction and model objects/3.R
@@ -1,1 +1,13 @@
 #3
+
+
+# Define a tibble where n_convenience is -1
+minus_one <- tibble(n_convenience = -1)
+
+# Define a tibble where n_convenience is 2.5
+two_pt_five <- tibble(n_convenience = 2.5)
+
+
+
+
+DM.result = 3
