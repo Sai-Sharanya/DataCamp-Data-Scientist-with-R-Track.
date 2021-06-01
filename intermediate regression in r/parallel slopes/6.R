9b22c69ef6e10b52419cb23bf353b4ff8ae3f938
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 13:31:47 +0530

    Update 6.R

--- intermediate regression in r/parallel slopes/6.R
+++ intermediate regression in r/parallel slopes/6.R
@@ -1,1 +1,26 @@
 #6
+
+
+mdl_price_vs_conv %>% 
+  # Get the model-level coefficients
+  glance() %>% 
+  # Pull out the RSE
+  pull(sigma)
+
+# Get the RSE for mdl_price_vs_age
+mdl_price_vs_age %>% 
+  glance()  %>%
+   pull(sigma)
+
+
+# Get the RSE for mdl_price_vs_both
+mdl_price_vs_both %>% 
+  glance()  %>%
+   pull(sigma)
+
+
+
+
+
+
+
