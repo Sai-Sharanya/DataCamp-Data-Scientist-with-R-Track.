e9225d15c1e9a0733974a43d0c6307dd60d2d437
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 10:08:45 +0530

    Update 2.R

--- Introduction to regression in R/Simple logistic regression/2.R
+++ Introduction to regression in R/Simple logistic regression/2.R
@@ -1,1 +1,23 @@
 #2
+
+
+
+# Using churn plot has_churned vs. time_since_first_purchase
+ggplot(churn, aes(time_since_first_purchase, has_churned)) +
+  # Make it a scatter plot
+  geom_point() +
+  # Add an lm trend line, no std error ribbon, colored red
+  geom_smooth(method = "lm", se = FALSE, color = "red")
+
+
+
+
+ggplot(churn, aes(time_since_first_purchase, has_churned)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  # Add a glm trend line, no std error ribbon, binomial family
+  geom_smooth(
+    method = "glm", 
+    se = FALSE, 
+    method.args = list(family = binomial)
+  )
