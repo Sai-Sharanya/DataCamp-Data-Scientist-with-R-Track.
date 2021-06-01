5882abcdb6da6e9bf7194126c9bc1c9d4c84a233
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:08:09 +0530

    Update 4.R

--- Introduction to regression in R/Simple linear regression/4.R
+++ Introduction to regression in R/Simple linear regression/4.R
@@ -1,1 +1,11 @@
 #4
+
+
+summary_stats <- taiwan_real_estate %>% 
+  # Group by house age
+  group_by(house_age_years) %>% 
+  # Summarize to calculate the mean house price/area
+  summarize(mean_by_group = mean(price_twd_msq))
+
+# See the result
+summary_stats
