13e3393594c37d2881b3e545720ec562c7237f13
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:34:07 +0530

    Update 1.R

--- Introduction to regression in R/Assessing model fit/1.R
+++ Introduction to regression in R/Assessing model fit/1.R
@@ -1,1 +1,36 @@
 #1
+
+
+# Print a summary of mdl_click_vs_impression_orig
+summary(mdl_click_vs_impression_orig)
+
+# Print a summary of mdl_click_vs_impression_trans
+summary(mdl_click_vs_impression_trans)
+
+
+
+
+
+# Get coeff of determination for mdl_click_vs_impression_orig
+mdl_click_vs_impression_orig %>% 
+  # Get the model-level details
+  glance() %>% 
+  # Pull out r.squared
+  pull(r.squared)
+
+# Do the same for the transformed model
+mdl_click_vs_impression_trans %>%
+   glance()   %>%
+     pull(r.squared)
+
+
+
+
+
+
+#The number of impressions explains 89% of the variability in the number of clicks.
+
+
+
+
+
