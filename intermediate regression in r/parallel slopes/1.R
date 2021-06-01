0dc9af35082b3578854a7f65e41d106d339fc076
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 10:59:47 +0530

    Update 1.R

--- intermediate regression in r/parallel slopes/1.R
+++ intermediate regression in r/parallel slopes/1.R
@@ -1,1 +1,26 @@
-#1
+#1 
+
+
+# Fit a linear regr'n of price_twd_msq vs. n_convenience
+mdl_price_vs_conv <- lm(price_twd_msq ~ n_convenience, data = taiwan_real_estate)
+
+# See the result
+mdl_price_vs_conv
+
+
+
+# Fit a linear regr'n of price_twd_msq vs. house_age_years, no intercept
+mdl_price_vs_age <- lm(price_twd_msq ~ house_age_years + 0, data = taiwan_real_estate)
+
+# See the result
+mdl_price_vs_age
+
+
+
+
+# Fit a linear regr'n of price_twd_msq vs. n_convenience 
+# plus house_age_years, no intercept
+mdl_price_vs_both <- lm(price_twd_msq ~ n_convenience + house_age_years + 0, data = taiwan_real_estate)
+
+# See the result
+mdl_price_vs_both
