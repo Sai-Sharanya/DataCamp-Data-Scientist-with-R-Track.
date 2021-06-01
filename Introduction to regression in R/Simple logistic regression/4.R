c6f6039960749597c0dad903807e0d3891854238
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 10:14:22 +0530

    Update 4.R

--- Introduction to regression in R/Simple logistic regression/4.R
+++ Introduction to regression in R/Simple logistic regression/4.R
@@ -1,1 +1,29 @@
 #4
+
+
+# Make a data frame of predicted probabilities
+prediction_data <- explanatory_data %>% 
+  mutate(   
+    has_churned = predict(
+      mdl_churn_vs_relationship, 
+      explanatory_data, 
+      type = "response"
+    )
+  )
+
+# See the result
+prediction_data
+
+
+
+
+# From previous step
+prediction_data <- explanatory_data %>% 
+  mutate(   
+    has_churned = predict(mdl_churn_vs_relationship, explanatory_data, type = "response")
+  )
+
+# Update the plot
+plt_churn_vs_relationship +
+  # Add points from prediction_data, colored yellow, size 2
+  geom_point(data = prediction_data, color = "yellow", size = 2)
