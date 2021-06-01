43cc4e71af5e4382ca9785abece528712564ea1a
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 13:49:36 +0530

    Update 3.R

--- intermediate regression in r/interactions/3.R
+++ intermediate regression in r/interactions/3.R
@@ -1,1 +1,25 @@
 #3
+
+
+
+# Using taiwan_real_estate, plot price vs. no. of conv. stores
+# colored by house age
+ggplot(taiwan_real_estate, aes(n_convenience, price_twd_msq, color = house_age_years)) +
+  # Make it a scatter plot
+  geom_point() +
+  # Add smooth linear regression trend lines, no ribbon
+  geom_smooth(method = "lm", se = FALSE)
+
+
+
+
+# Extend the plot to include prediction points
+ggplot(taiwan_real_estate, aes(n_convenience, price_twd_msq, color = house_age_years)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  # Add points using prediction_data_0_to_15, colored red, size 3, shape 15
+  geom_point(data = prediction_data_0_to_15, color = "red", size = 3, shape = 15) +
+  # Add points using prediction_data_15_to_30, colored green, size 3, shape 15
+  geom_point(data = prediction_data_15_to_30, color = "green", size = 3, shape = 15) +
+  # Add points using prediction_data_30_to_45, colored blue, size 3, shape 15
+  geom_point(data = prediction_data_30_to_45, color = "blue", size = 3, shape = 15)
