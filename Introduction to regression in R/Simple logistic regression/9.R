28793711be3c604c0685c01602d829f53e179fc8
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 10:21:18 +0530

    Update 9.R

--- Introduction to regression in R/Simple logistic regression/9.R
+++ Introduction to regression in R/Simple logistic regression/9.R
@@ -1,1 +1,12 @@
 #9
+
+
+
+# Convert outcomes to a yardstick confusion matrix
+confusion <- conf_mat(outcomes)
+
+# Plot the confusion matrix
+autoplot(confusion)
+
+# Get performance metrics for the confusion matrix
+summary(confusion, event_level = "second")
