accb1d221871dcc8cb854b9fa74cd27bb4cf3fda
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 16:45:50 +0530

    Update 9.R

--- intermediate regression in r/interactions/9.R
+++ intermediate regression in r/interactions/9.R
@@ -1,1 +1,21 @@
 #9
+
+
+
+# Take a glimpse at the dataset
+glimpse(auctions)
+
+# Model price vs. opening bid using auctions
+mdl_price_vs_openbid <- lm(price ~ openbid, data = auctions)
+
+# See the result
+mdl_price_vs_openbid
+
+
+
+
+# Using auctions, plot price vs. opening bid as a 
+# scatter plot with linear regression trend lines
+ggplot(auctions, aes(openbid, price)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
