a304bc4569d043c461f2112ee16d53175125a9d1
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:17:17 +0530

    Update 6.R

--- Introduction to regression in R/Prediction and model objects/6.R
+++ Introduction to regression in R/Prediction and model objects/6.R
@@ -1,1 +1,16 @@
 #6
+
+
+# Get the coefficient-level elements of the model
+tidy(mdl_price_vs_conv)
+
+
+
+# Get the observation-level elements of the model
+augment(mdl_price_vs_conv)
+
+
+
+
+# Get the model-level elements of the model
+glance(mdl_price_vs_conv)
