effdaa8b09e6b9ccad7b7660dcfd39780c516c3e
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 10:20:58 +0530

    Update 8.R

--- Introduction to regression in R/Simple logistic regression/8.R
+++ Introduction to regression in R/Simple logistic regression/8.R
@@ -1,1 +1,15 @@
 #8
+
+
+
+# Get the actual responses from the dataset
+actual_response <- churn$has_churned
+
+# Get the "most likely" predicted responses from the model
+predicted_response <- round(fitted(mdl_churn_vs_relationship))
+
+# Create a table of counts
+outcomes <- table(predicted_response, actual_response)
+
+# See the result
+outcomes
