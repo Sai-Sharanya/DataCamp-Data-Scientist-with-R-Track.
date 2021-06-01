8f79daced9f41dab76fcc060998aad26072e988d
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:08:51 +0530

    Update 5.R

--- Introduction to regression in R/Simple linear regression/5.R
+++ Introduction to regression in R/Simple linear regression/5.R
@@ -1,1 +1,22 @@
 #5
+
+
+# Run a linear regression of price_twd_msq vs. house_age_years
+mdl_price_vs_age <- lm(
+  price_twd_msq ~ house_age_years, 
+  data = taiwan_real_estate
+)
+
+# See the result
+mdl_price_vs_age
+
+
+
+# Update the model formula to remove the intercept
+mdl_price_vs_age_no_intercept <- lm(
+  price_twd_msq ~ house_age_years + 0, 
+  data = taiwan_real_estate
+)
+
+# See the result
+mdl_price_vs_age_no_intercept
