5a0060a821b6fa93231e185df55d0b94188ff88e
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 10:09:28 +0530

    Update 3.R

--- Introduction to regression in R/Simple logistic regression/3.R
+++ Introduction to regression in R/Simple logistic regression/3.R
@@ -1,1 +1,13 @@
 #3
+
+
+# Fit a logistic regression of churn vs. 
+# length of relationship using the churn dataset
+mdl_churn_vs_relationship <- glm(
+  has_churned ~ time_since_first_purchase, 
+  data = churn, 
+  family = binomial
+)
+
+# See the result
+mdl_churn_vs_relationship
