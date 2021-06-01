09549d5d4ab6aa00ed3ca0bceadb4f7fd8ac35b5
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 13:43:10 +0530

    Update 1.R

--- intermediate regression in r/interactions/1.R
+++ intermediate regression in r/interactions/1.R
@@ -1,1 +1,48 @@
 #1
+
+
+
+# Filter for rows where house age is 0 to 15 years
+taiwan_0_to_15 <- taiwan_real_estate %>%
+  filter(house_age_years == "0 to 15")
+
+# Filter for rows where house age is 15 to 30 years
+taiwan_15_to_30 <- taiwan_real_estate %>%
+  filter(house_age_years == "15 to 30")
+
+# Filter for rows where house age is 30 to 45 years
+taiwan_30_to_45 <- taiwan_real_estate %>%
+  filter(house_age_years == "30 to 45")
+
+
+
+
+
+
+# From previous step
+taiwan_0_to_15 <- taiwan_real_estate %>%
+  filter(house_age_years == "0 to 15")
+taiwan_15_to_30 <- taiwan_real_estate %>%
+  filter(house_age_years == "15 to 30")
+taiwan_30_to_45 <- taiwan_real_estate %>%
+  filter(house_age_years == "30 to 45")
+
+# Model price vs. no. convenience stores using 0 to 15 data
+mdl_0_to_15 <- lm(price_twd_msq ~ n_convenience, data = taiwan_0_to_15)
+
+# Model price vs. no. convenience stores using 15 to 30 data
+mdl_15_to_30 <- lm(price_twd_msq ~ n_convenience, data = taiwan_15_to_30)
+
+# Model price vs. no. convenience stores using 30 to 45 data
+mdl_30_to_45 <- lm(price_twd_msq ~ n_convenience, data = taiwan_30_to_45)
+
+# See the results
+mdl_0_to_15
+mdl_15_to_30
+mdl_30_to_45
+
+
+
+
+
+
