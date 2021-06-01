524d777954d3754ac569a90db7fd05db98b8ecb5
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 15:30:58 +0530

    Update 4.R

--- intermediate regression in r/interactions/4.R
+++ intermediate regression in r/interactions/4.R
@@ -1,1 +1,47 @@
 #4
+
+
+# Get the coeff. of determination for mdl_all_ages
+mdl_all_ages %>% 
+  glance() %>% 
+  pull(r.squared)
+
+# Get the coeff. of determination for mdl_0_to_15
+mdl_0_to_15 %>% 
+  glance() %>% 
+  pull(r.squared)
+
+# Get the coeff. of determination for mdl_15_to_30
+mdl_15_to_30 %>% 
+  glance() %>% 
+  pull(r.squared)
+
+# Get the coeff. of determination for mdl_30_to_45
+mdl_30_to_45 %>% 
+  glance() %>% 
+  pull(r.squared)
+
+
+
+
+
+
+# Get the RSE for mdl_all
+mdl_all_ages %>% 
+  glance() %>% 
+  pull(sigma)
+
+# Get the RSE for mdl_0_to_15
+mdl_0_to_15 %>% 
+  glance() %>% 
+  pull(sigma)
+
+# Get the RSE for mdl_15_to_30
+mdl_15_to_30 %>% 
+  glance() %>% 
+  pull(sigma)
+
+# Get the RSE for mdl_30_to_45
+mdl_30_to_45 %>% 
+  glance() %>% 
+  pull(sigma)
