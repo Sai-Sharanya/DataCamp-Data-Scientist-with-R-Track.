465d6e67682f04b128d7c858e72263915f0d97bf
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 10:17:37 +0530

    Update 7.R

--- Introduction to regression in R/Simple logistic regression/7.R
+++ Introduction to regression in R/Simple logistic regression/7.R
@@ -1,1 +1,34 @@
 #7
+
+
+# Update the data frame
+prediction_data <- explanatory_data %>% 
+  mutate(   
+    has_churned = predict(mdl_churn_vs_relationship, explanatory_data, type = "response"),
+    odds_ratio = has_churned / (1 - has_churned),
+    # Add the log odds ratio from odds_ratio
+    log_odds_ratio = log(odds_ratio),
+    # Add the log odds ratio using predict()
+    log_odds_ratio2 = predict(mdl_churn_vs_relationship, explanatory_data)
+  )
+
+# See the result
+prediction_data
+
+
+
+
+# From previous step
+prediction_data <- explanatory_data %>% 
+  mutate(   
+    has_churned = predict(mdl_churn_vs_relationship, explanatory_data, type = "response"),
+    odds_ratio = has_churned / (1 - has_churned),
+    log_odds_ratio = log(odds_ratio)
+  )
+
+# Update the plot
+ggplot(prediction_data, aes(time_since_first_purchase, odds_ratio)) +
+  geom_line() +
+  geom_hline(yintercept = 1, linetype = "dotted") +
+  # Use a logarithmic y-scale
+  scale_y_log10()
