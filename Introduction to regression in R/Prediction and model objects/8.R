dcc034b73e97e1e165fa42f8ed05b9533646dae6
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:19:09 +0530

    Update 8.R

--- Introduction to regression in R/Prediction and model objects/8.R
+++ Introduction to regression in R/Prediction and model objects/8.R
@@ -1,1 +1,30 @@
 #8
+
+
+# Run a linear regression on return_2019 vs. return_2018
+# using sp500_yearly_returns
+mdl_returns <- lm(formula = return_2019~return_2018, data= sp500_yearly_returns)
+
+
+
+
+# See the result
+mdl_returns
+
+
+
+
+
+# From previous step
+mdl_returns <- lm(
+  return_2019 ~ return_2018, 
+  data = sp500_yearly_returns
+)
+
+# Create a data frame with return_2018 at -1, 0, and 1 
+explanatory_data <- tibble(
+  return_2018 = c(-1, 0, 1)
+)
+
+# Use mdl_returns to predict with explanatory_data
+predict(mdl_returns, explanatory_data)
