398018ffc9850db0861d39837fb6a491d1d9c3a6
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 16:56:58 +0530

    Update 10.R

--- intermediate regression in r/interactions/10.R
+++ intermediate regression in r/interactions/10.R
@@ -1,1 +1,15 @@
 #10
+
+
+# Fit linear regression of price vs. opening bid and auction 
+# type, with an interaction.
+mdl_price_vs_both <- lm(
+  price ~ auction_type + openbid:auction_type + 0, # or price ~ auction_type * openbid
+  data = auctions
+)
+
+# See the result
+mdl_price_vs_both
+
+
+
