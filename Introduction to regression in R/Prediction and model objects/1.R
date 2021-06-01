e9fdfbc364b90d458c7fc36f990149cfb8160a6b
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:13:31 +0530

    Update 1.R

--- Introduction to regression in R/Prediction and model objects/1.R
+++ Introduction to regression in R/Prediction and model objects/1.R
@@ -1,1 +1,33 @@
 #1
+
+
+# Create a tibble with n_convenience column from zero to ten
+explanatory_data <- tibble(
+  n_convenience = 0:10
+)
+
+
+# From previous step
+explanatory_data <- tibble(
+  n_convenience = 0:10
+)
+
+# Use mdl_price_vs_conv to predict with explanatory_data
+predict(mdl_price_vs_conv, explanatory_data)
+
+
+
+
+# From previous steps
+explanatory_data <- tibble(
+  n_convenience = 0:10
+)
+
+# Edit this, so predictions are stored in prediction_data
+prediction_data <- explanatory_data %>% 
+  mutate(
+    price_twd_msq = predict(mdl_price_vs_conv, explanatory_data)
+  )
+
+# See the result
+prediction_data
