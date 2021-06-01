7806bbb331576e5dedeebd4d12b5238829a4547b
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 11:51:05 +0530

    Update 3.R

--- intermediate regression in r/parallel slopes/3.R
+++ intermediate regression in r/parallel slopes/3.R
@@ -1,1 +1,10 @@
 #3
+
+
+# Using taiwan_real_estate, plot price_twd_msq vs. n_convenience
+# colored by house_age_years
+ggplot(taiwan_real_estate, aes(n_convenience, price_twd_msq, color = house_age_years)) +
+  # Add a point layer
+  geom_point() +
+  # Add parallel slopes, no ribbon
+  geom_parallel_slopes(se = FALSE)
