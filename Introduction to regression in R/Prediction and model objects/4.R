f1421fd70eec90eec4d3e87b6ad3ddf001a5e945
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 09:15:45 +0530

    Update 4.R

--- Introduction to regression in R/Prediction and model objects/4.R
+++ Introduction to regression in R/Prediction and model objects/4.R
@@ -1,1 +1,21 @@
 #4
+
+
+# Get the model coefficients of mdl_price_vs_conv
+coefficients(mdl_price_vs_conv)
+
+
+
+# Get the fitted values of mdl_price_vs_conv
+fitted(mdl_price_vs_conv)
+
+
+
+
+# Get the residuals of mdl_price_vs_conv
+residuals(mdl_price_vs_conv)
+
+
+
+# Print a summary of mdl_price_vs_conv
+summary(mdl_price_vs_conv)
