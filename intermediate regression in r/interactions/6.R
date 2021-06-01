bb046839619b4acea20b2c8c5110a86e7d0062ea
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-01 16:15:14 +0530

    Update 6.R

--- intermediate regression in r/interactions/6.R
+++ intermediate regression in r/interactions/6.R
@@ -1,1 +1,25 @@
 #6
+
+
+# Model price vs. house age plus an interaction, no intercept
+mdl_readable_inter <- lm(
+  price_twd_msq ~ house_age_years + n_convenience:house_age_years + 0, 
+  data = taiwan_real_estate
+)
+
+# See the result
+mdl_readable_inter
+
+# Get coefficients for mdl_0_to_15
+coefficients(mdl_0_to_15)
+
+# Get coefficients for mdl_15_to_30
+coefficients(mdl_15_to_30)
+
+# Get coefficients for mdl_30_to_45
+coefficients(mdl_30_to_45)
+
+
+
+
+
