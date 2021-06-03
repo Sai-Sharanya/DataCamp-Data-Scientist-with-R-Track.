7868b066a8111890d340644abee11d5c74d14680
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-03 07:27:51 +0530

    Update 3.R

--- intermediate regression in r/multiple linear regression/3.R
+++ intermediate regression in r/multiple linear regression/3.R
@@ -1,1 +1,54 @@
 #3
+
+
+# Fit a linear regression of price vs. no. of conv. stores
+# and sqrt dist. to nearest MRT, with interaction
+mdl_price_vs_conv_dist <- lm(
+  price_twd_msq ~ n_convenience * sqrt(dist_to_mrt_m), data = taiwan_real_estate
+)
+
+# See the result
+mdl_price_vs_conv_dist
+
+
+
+
+
+# From previous step 
+mdl_price_vs_conv_dist <- lm(price_twd_msq ~ n_convenience * sqrt(dist_to_mrt_m), data = taiwan_real_estate)
+
+# Create expanded grid of explanatory variables with
+# no. of conv. stores and  dist. to nearest MRT
+explanatory_data <- expand_grid(
+  n_convenience = 0:10,
+  dist_to_mrt_m = seq(0, 80, 10) ^ 2
+)
+
+# Add predictions using mdl_price_vs_conv_dist and explanatory_data
+prediction_data <- explanatory_data %>% 
+  mutate(
+    price_twd_msq = predict(mdl_price_vs_conv_dist, explanatory_data)
+  )
+
+# See the result
+prediction_data
+
+
+
+
+
+# From previous steps
+mdl_price_vs_conv_dist <- lm(price_twd_msq ~ n_convenience * sqrt(dist_to_mrt_m), data = taiwan_real_estate)
+explanatory_data <- expand_grid(n_convenience = 0:10, dist_to_mrt_m = seq(0, 80, 10) ^ 2)
+prediction_data <- explanatory_data %>% 
+  mutate(price_twd_msq = predict(mdl_price_vs_conv_dist, explanatory_data))
+
+# Add predictions to plot
+ggplot(
+  taiwan_real_estate, 
+  aes(n_convenience, sqrt(dist_to_mrt_m), color = price_twd_msq)
+) + 
+  geom_point() +
+  scale_color_viridis_c(option = "plasma")+
+  # Add prediction points colored yellow, size 3
+  geom_point(data = prediction_data, color = "yellow", size = 3)
