23e717e049ed545886c567d44cf0a7b9eabf9515
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-03 07:56:15 +0530

    Update 6.R

--- intermediate regression in r/multiple logistics regression/6.R
+++ intermediate regression in r/multiple logistics regression/6.R
@@ -1,1 +1,30 @@
 #6
+
+logistic_distn_inv_cdf <- tibble(
+  # Make a seq from 0.001 to 0.999 in steps of 0.001
+  p = seq(0.001, 0.999, 0.001),
+  # Transform with built-in logistic inverse CDF
+  logit_p = qlogis(p),
+  # Transform with manual logit
+  logit_p_man = log(p / (1 - p))
+) 
+
+# Check that each logistic function gives the same results
+all.equal(
+  logistic_distn_inv_cdf$logit_p,
+  logistic_distn_inv_cdf$logit_p_man
+)
+
+
+
+# From previous step
+logistic_distn_inv_cdf <- tibble(
+  p = seq(0.001, 0.999, 0.001),
+  logit_p = qlogis(p),
+  logit_p_man = log(p / (1 - p))
+)
+
+# Using logistic_distn_inv_cdf, plot logit_p vs. p
+ggplot(logistic_distn_inv_cdf, aes(p, logit_p)) +
+  # Make it a line plot
+  geom_line()
