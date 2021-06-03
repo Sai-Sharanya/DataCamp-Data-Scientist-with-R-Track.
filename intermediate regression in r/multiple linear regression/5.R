8d67d51d573c480dfbaf50ce211ed0b84351f232
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-03 07:33:16 +0530

    Update 5.R

--- intermediate regression in r/multiple linear regression/5.R
+++ intermediate regression in r/multiple linear regression/5.R
@@ -1,1 +1,43 @@
 #5
+
+
+# Model price vs. no. of conv. stores, sqrt dist. to MRT 
+# station & house age, no global intercept, no interactions
+mdl_price_vs_all_no_inter <- lm(
+  price_twd_msq ~ n_convenience + sqrt(dist_to_mrt_m) + house_age_years + 0, 
+  data = taiwan_real_estate
+)
+
+# See the result
+mdl_price_vs_all_no_inter
+
+
+
+
+
+
+# Model price vs. sqrt dist. to MRT station, no. of conv.
+# stores & house age, no global intercept, 3-way interactions
+mdl_price_vs_all_3_way_inter <- lm(
+  price_twd_msq ~ sqrt(dist_to_mrt_m) * n_convenience * house_age_years + 0, 
+  data = taiwan_real_estate
+)
+
+# See the result
+mdl_price_vs_all_3_way_inter
+
+
+
+
+
+
+
+# Model price vs. sqrt dist. to MRT station, no. of conv.
+# stores & house age, no global intercept, 2-way interactions
+mdl_price_vs_all_2_way_inter <- lm(
+  price_twd_msq ~ (sqrt(dist_to_mrt_m) + n_convenience + house_age_years) ^ 2 + 0, 
+  data = taiwan_real_estate
+)
+
+# See the result
+mdl_price_vs_all_2_way_inter
