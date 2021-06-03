2eafe52be30dadbbedd66e71aefc8f78703ddd3a
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-03 07:46:39 +0530

    Update 1.R

--- intermediate regression in r/multiple logistics regression/1.R
+++ intermediate regression in r/multiple logistics regression/1.R
@@ -1,1 +1,14 @@
 #1
+
+# Using churn, plot recency vs. length of relationship,
+# colored by churn status
+ggplot(
+  churn, 
+  aes(time_since_first_purchase, time_since_last_purchase, color = has_churned)
+) +
+  # Make it a scatter plot, with transparency 0.5
+  geom_point(alpha = 0.5) +
+  # Use a 2-color gradient split at 0.5
+  scale_color_gradient2(midpoint = 0.5) +
+  # Use the black and white theme
+  theme_bw()
