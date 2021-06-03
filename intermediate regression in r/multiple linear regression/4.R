ce4c57e7722e2f0d265e27ef38e100149202376f
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-03 07:32:16 +0530

    Update 4.R

--- intermediate regression in r/multiple linear regression/4.R
+++ intermediate regression in r/multiple linear regression/4.R
@@ -1,1 +1,15 @@
 #4
+
+
+# Using taiwan_real_estate, no. of conv. stores vs. sqrt of
+# dist. to MRT, colored by plot house price
+ggplot(
+  taiwan_real_estate, 
+  aes(sqrt(dist_to_mrt_m), n_convenience, color = price_twd_msq)
+) +
+  # Make it a scatter plot
+  geom_point() +
+  # Use the continuous viridis plasma color scale
+  scale_color_viridis_c(option = "plasma") +
+  # Facet, wrapped by house age
+  facet_wrap(vars(house_age_years))
