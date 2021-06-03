d944cea98c58f803d1c46260c7139f59c3aec566
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-03 07:41:37 +0530

    Update 7.R

--- intermediate regression in r/multiple linear regression/7.R
+++ intermediate regression in r/multiple linear regression/7.R
@@ -1,1 +1,59 @@
 #7
+
+
+# Set the intercept to 10
+intercept <- 10
+
+# Set the slope to 1
+slope <- 1
+
+# Calculate the predicted y values
+y_pred <- intercept + slope * x_actual
+
+# Calculate the differences between actual and predicted
+y_diff <- y_actual - y_pred
+
+# Calculate the sum of squares
+sum(y_diff ^ 2)
+
+
+
+calc_sum_of_squares <- function(coeffs) {
+  # Get the intercept coeff
+  intercept <- coeffs[1]
+
+  # Get the slope coeff
+  slope <- coeffs[2]
+
+  # Calculate the predicted y values
+  y_pred <- intercept + slope * x_actual
+
+  # Calculate the differences between actual and predicted
+  y_diff <- y_actual - y_pred
+
+  # Calculate the sum of squares
+  sum(y_diff ^ 2)
+}
+
+
+
+
+# From previous step
+calc_sum_of_squares <- function(coeffs) {
+  intercept <- coeffs[1]
+  slope <- coeffs[2]
+  y_pred <- intercept + slope * x_actual
+  y_diff <- y_actual - y_pred
+  sum(y_diff ^ 2)
+}
+
+# Optimize the metric
+optim(
+  # Initially guess 0 intercept and 0 slope
+  par = c(intercept = 0, slope = 0), 
+  # Use calc_sum_of_squares as the optimization fn 
+  fn = calc_sum_of_squares
+)
+
+# Compare the coefficients to those calculated by lm()
+lm(price_twd_msq ~ n_convenience, data = taiwan_real_estate)
