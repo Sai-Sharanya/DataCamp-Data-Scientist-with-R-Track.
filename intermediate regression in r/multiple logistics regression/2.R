6f4db60d4ecccd62030c0e91a535da44a0bf62d1
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-03 07:47:22 +0530

    Update 2.R

--- intermediate regression in r/multiple logistics regression/2.R
+++ intermediate regression in r/multiple logistics regression/2.R
@@ -1,1 +1,13 @@
 #2
+
+
+# Fit a logistic regression of churn status vs. length of
+# relationship, recency, and an interaction
+mdl_churn_vs_both_inter <- glm(
+  has_churned ~ time_since_first_purchase * time_since_last_purchase, 
+  data = churn, 
+  family = binomial
+)
+
+# See the result
+mdl_churn_vs_both_inter
