ec49900db2b160091369941d2bdc2fa7c2a074b4
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-03 07:56:54 +0530

    Update 7.R

--- intermediate regression in r/multiple logistics regression/7.R
+++ intermediate regression in r/multiple logistics regression/7.R
@@ -1,1 +1,16 @@
 #7
+
+# Look at the structure of binomial() function
+str(binomial())
+
+# Call the link inverse on x
+linkinv_x <- binomial()$linkinv(x)
+
+# Check linkinv_x and plogis() of x give same results 
+all.equal(linkinv_x, plogis(x))
+
+# Call the link fun on p
+linkfun_p <- binomial()$linkfun(p)
+
+# Check linkfun_p and qlogis() of p give same results 
+all.equal(linkfun_p, qlogis(p))
