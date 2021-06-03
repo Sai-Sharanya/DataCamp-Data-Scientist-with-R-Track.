7204661d5c60f3ace87c78afbbd630f071a9549b
Sai Sharanya Y <63389382+SaiSharanyaY@users.noreply.github.com>
2021-06-03 07:24:13 +0530

    Update 1.R

--- intermediate regression in r/multiple linear regression/1.R
+++ intermediate regression in r/multiple linear regression/1.R
@@ -1,1 +1,21 @@
 #1
+
+
+# With taiwan_real_estate, draw a 3D scatter plot of
+# no. of conv. stores, sqrt dist to MRT, and price
+taiwan_real_estate %$% 
+  scatter3D(n_convenience, sqrt(dist_to_mrt_m), price_twd_msq)
+
+
+
+
+# Using taiwan_real_estate, plot sqrt dist to MRT vs. 
+# no. of conv. stores, colored by price
+ggplot(
+  taiwan_real_estate, 
+  aes(n_convenience, sqrt(dist_to_mrt_m), color = price_twd_msq)
+) + 
+  # Make it a scatter plot
+  geom_point() +
+  # Use the continuous viridis plasma color scale
+  scale_color_viridis_c(option = "plasma")
